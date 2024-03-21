c5b343fdaeebd81b134cf662e0999183a21c41b5
AarooshRBLX <164038746+Aarooshsaifanboy322@users.noreply.github.com>
2024-03-21 17:47:19 +0600

    Add files via upload

---
+++ Chatter.js
@@ -0,0 +1,4 @@
+function account_remainder() {
+    let account_created = window.alert("You have successfully created an account!")
+    window.open("file:///Users/amranhossain/Desktop/Coding/HTML/Chatter/Main%20page/index.html")
+}
